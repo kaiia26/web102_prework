d3c18f7017b4ddab3674ec1957b2b7409fe64aa8
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2025-01-07 19:05:29 -0500

    Returning games in an array format

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
+    for (let i = 0; i < games.length; i++) {
+        console.log(games[i])
+    }
+        
 
 
         // create a new div element, which will become the game card
